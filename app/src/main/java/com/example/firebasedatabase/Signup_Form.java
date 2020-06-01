4568ec9056549f2438f20b4579613a5441548f54
sidharth nayyar <sidnayyar1@gmail.com>
2020-06-01 09:43:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/firebasedatabase/Signup_Form.java
@@ -0,0 +1,17 @@
+package com.example.firebasedatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup__form);
+        getSupportActionBar().setTitle("Signup Form");
+    }
+
+}
