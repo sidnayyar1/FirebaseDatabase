a8ac13cc3b0d5b071fd47652fc1a24920507f832
sidharth nayyar <sidnayyar1@gmail.com>
2020-07-24 10:07:53 -0400

    working on login activity

--- app/src/main/java/com/example/firebasedatabase/Login_Form.java
+++ app/src/main/java/com/example/firebasedatabase/Login_Form.java
@@ -1,19 +1,75 @@
 package com.example.firebasedatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login_Form extends AppCompatActivity {
 
+    EditText txtemail,txtpassword;
+    Button btnlogin;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__form);
         getSupportActionBar().setTitle("Login Form");
+
+        txtemail=(EditText)findViewById(R.id.username);
+        txtpassword=(EditText)findViewById(R.id.password);
+        btnlogin=(Button)findViewById(R.id.btnlogin);
+
+firebaseAuth=FirebaseAuth.getInstance();
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String email=txtemail.getText().toString().trim();
+            String password=txtpassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(Login_Form.this,"please enter email",Toast.LENGTH_LONG).show();
+                return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(Login_Form.this,"please enter password",Toast.LENGTH_LONG).show();
+               return;
+                }
+
+
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login_Form.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                } else {
+
+                                }
+
+                                // ...
+                            }
+                        });
+
+
+
+            }
+        });
+
+
     }
+
 
     public void btn_signup_Form(View view) {
         startActivity(new Intent(getApplicationContext(),Signup_Form.class));
