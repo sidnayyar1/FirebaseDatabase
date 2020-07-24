0d4e512609c872aab70e1ed1f6edf67945eb39e0
sidharth nayyar <sidnayyar1@gmail.com>
2020-07-24 16:32:16 -0400

    phone verification and credentials code in PhoneVerification java file.

--- app/src/main/java/com/example/firebasedatabase/PhoneVerification.java
+++ app/src/main/java/com/example/firebasedatabase/PhoneVerification.java
@@ -3,13 +3,20 @@ package com.example.firebasedatabase;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -19,17 +26,20 @@ public class PhoneVerification extends AppCompatActivity {
 
     EditText phoneVerifynum,verifycode;
     Button phoneNumber,singin;
-FirebaseAuth firebaseAuth;
+    FirebaseAuth firebaseAuth;
+    String codeSent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_verification);
-    phoneVerifynum=(EditText)findViewById(R.id.phoneNumber);
-    verifycode=(EditText)findViewById(R.id.verificationcode);
+        phoneVerifynum=(EditText)findViewById(R.id.phoneNumber);
+        verifycode=(EditText)findViewById(R.id.verificationcode);
+        phoneNumber=(Button)findViewById(R.id.btnphoneverifycode);
+        singin = (Button)findViewById(R.id.btnsignin);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-phoneNumber=(Button)findViewById(R.id.btnphoneverifycode);
-singin = (Button)findViewById(R.id.btnsignin);
-firebaseAuth = FirebaseAuth.getInstance();
+
+
 phoneNumber.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
@@ -40,11 +50,31 @@ singin.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
 
+        verifySignInCode();
     }
 });
+    }
+    private void verifySignInCode(){
+        String code=verifycode.getText().toString();
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
 
+                                Toast.makeText(PhoneVerification.this,"Invalid number",Toast.LENGTH_LONG).show();
+                            }
 
-
+                        }
+                    }
+                });
     }
 
     private void sendVerificationCode(){
@@ -79,6 +109,13 @@ phoneVerifynum.requestFocus();
         @Override
         public void onVerificationFailed(@NonNull FirebaseException e) {
 
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+        codeSent =s;
         }
     };
 }
