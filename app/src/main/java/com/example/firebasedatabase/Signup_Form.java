601d2c0c23e331010dc4d07bbc52d289d551448f
sidharth nayyar <sidnayyar1@gmail.com>
2020-07-24 09:39:15 -0400

    working on the text fields

--- app/src/main/java/com/example/firebasedatabase/Signup_Form.java
+++ app/src/main/java/com/example/firebasedatabase/Signup_Form.java
@@ -1,13 +1,20 @@
 package com.example.firebasedatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,6 +25,7 @@ RadioButton btnMale;
 RadioButton btnFemale;
 DatabaseReference databaseReference;
     String gender = "";
+    FirebaseAuth firebaseAuth;
 //FirebaseDatabase firebaseDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +43,7 @@ DatabaseReference databaseReference;
         btnRegister=(Button)findViewById(R.id.btnregister);
 
         databaseReference=FirebaseDatabase.getInstance().getReference("Customer");
-
+    firebaseAuth = FirebaseAuth.getInstance();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +60,36 @@ DatabaseReference databaseReference;
                 if(btnFemale.isChecked()){
                     gender = "Female";
                 }
-                
+
+           if (TextUtils.isEmpty(email)){
+               Toast.makeText(Signup_Form.this,"please enter email",Toast.LENGTH_LONG).show();
+           }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(Signup_Form.this,"please enter password",Toast.LENGTH_LONG).show();
+                }
+                if (TextUtils.isEmpty(username)){
+                    Toast.makeText(Signup_Form.this,"please enter username",Toast.LENGTH_LONG).show();
+
+                }
+                if (TextUtils.isEmpty(fullname)){
+                    Toast.makeText(Signup_Form.this,"please enter fullname",Toast.LENGTH_LONG).show();
+
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Signup_Form.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                } else {
+
+                                }
+
+                                // ...
+                            }
+                        });
+
             }
         });
     }
