0f8b253ae066006cf1a6eb787b367728e40f31d3
sidharth nayyar <sidnayyar1@gmail.com>
2020-07-24 09:22:10 -0400

    Added fields in signup page and created firebase reference

--- app/src/main/java/com/example/firebasedatabase/Signup_Form.java
+++ app/src/main/java/com/example/firebasedatabase/Signup_Form.java
@@ -3,15 +3,39 @@ package com.example.firebasedatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup_Form extends AppCompatActivity {
-
+EditText txtFullName,txtUserName,txtEmail,txtPassword;
+Button btnRegister;
+RadioButton btnMale;
+RadioButton btnFemale;
+DatabaseReference databaseReference;
+//FirebaseDatabase firebaseDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_signup__form);
         getSupportActionBar().setTitle("Signup Form");
+
+
+        txtFullName=(EditText)findViewById(R.id.fullname);
+        txtUserName=(EditText)findViewById(R.id.username);
+        txtEmail=(EditText)findViewById(R.id.emailid);
+        txtPassword=(EditText)findViewById(R.id.password);
+        btnMale=(RadioButton)findViewById(R.id.radiobtnmale);
+        btnFemale=(RadioButton)findViewById(R.id.radiobuttonfemale);
+        btnRegister=(Button)findViewById(R.id.btnregister);
+
+        databaseReference=FirebaseDatabase.getInstance().getReference("Customer");
+
+
     }
+
 
 }
---
+++ app/src/main/java/com/example/firebasedatabase/customer.java
@@ -0,0 +1,15 @@
+package com.example.firebasedatabase;
+
+public class customer {
+    public String FullName,UserName,Email,Gender;
+    public customer(){
+
+    }
+
+    public customer(String fullName, String userName, String email, String gender) {
+        FullName = fullName;
+        UserName = userName;
+        Email = email;
+        Gender = gender;
+    }
+}
